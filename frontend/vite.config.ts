f08443b5ffc18d3a1c2e89db639eb531ed64be92
naruto <narutopandit220@gmail.com>
2025-12-16 00:21:54 +0530

    other problem

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  define: {
+    'import.meta.env.VITE_BACKEND_URL': JSON.stringify(
+      process.env.VITE_BACKEND_URL || 'http://localhost:4000'
+    )
+  }
 })
